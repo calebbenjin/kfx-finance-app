93065b4ffd99273d1dbba8bcd112590d9a73a7c9
calebbenjin <calebben94@gmail.com>
2023-01-05 15:36:16 +0100

    ScrollAnimation Hooks

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -3,24 +3,28 @@ import FeatureImg from '../assets/Illustration.png'
 import Image from 'next/image'
 import styled from 'styled-components'
 import Link from 'next/link'
+import ScrollAnimation from './ScrollAnimation'
+
 
 const FeatureSection = () => {
   return (
-    <SectionWrapper>
-      <div className="container mx-auto py-20 grid grid-cols-1 md:grid-cols-1 md:gap-6 lg:grid-cols-2 lg:gap-2">
-        <div className="img-wrapper">
-          <Image src={FeatureImg} alt="FeatureImg" width={820} height={752} className="md:mt-20 xl:mt-4" />
-        </div>
-        <div className="content-wrapper text-center xl:text-left lg:pl-10 xl:pl-10 xl:pt-10 pt-10">
-          <h2 className="md:text-1xl font-bold capitalize text-blue">Our Feature</h2>
-          <h1 className="text-3xl my-5 lg:text-4xl xl:text-6xl md:my-4 xl:my-10 font-semibold tracking-normal">Receive payments quickly from anywhere</h1>
-          <div className="xl:mr-10 md:mb-10 mb-10">
-            <p className="text-lg text-gray-500">Why kept very ever home mrs. Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to. </p>
+    <ScrollAnimation>
+      <SectionWrapper>
+        <div className="container mx-auto py-20 grid grid-cols-1 md:grid-cols-1 md:gap-6 lg:grid-cols-2 lg:gap-2">
+          <div className="img-wrapper">
+            <Image src={FeatureImg} alt="FeatureImg" width={820} height={752} className="md:mt-20 xl:mt-4" />
           </div>
-          <Link href="/" className="core-btn shadow-2xl bg-blue text-gray-100 py-4 px-7 mr-4">Tack your money</Link>
+          <div className="content-wrapper text-center xl:text-left lg:pl-10 xl:pl-10 xl:pt-10 pt-10">
+            <h2 className="md:text-1xl font-bold capitalize text-blue">Our Feature</h2>
+            <h1 className="text-3xl my-5 lg:text-4xl xl:text-6xl md:my-4 xl:my-10 font-semibold tracking-normal">Receive payments quickly from anywhere</h1>
+            <div className="xl:mr-10 md:mb-10 mb-10">
+              <p className="text-lg text-gray-500">Why kept very ever home mrs. Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to. </p>
+            </div>
+            <Link href="/" className="core-btn shadow-2xl bg-blue text-gray-100 py-4 px-7 mr-4">Tack your money</Link>
+          </div>
         </div>
-      </div>
-    </SectionWrapper>
+      </SectionWrapper>
+    </ScrollAnimation>
   )
 }
 
