0c3ffdc5155744214d14e64b5cb2e1b6facaf165
calebbenjin <calebben94@gmail.com>
2023-01-19 14:07:29 +0100

    modified next.config

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = withPWA({
   },
   env: {
     REACT_APP_API_URL: 'https://payforeign-api-production.up.railway.app/api/v1/users/',
-    NEXT_APP_API_URL: 'http://localhost:3000/api',
+    NEXT_APP_API_URL: 'https://payforeign.vercel.app/api',
     REACT_APP_CURRENCY_API: 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies'
   }
 });
