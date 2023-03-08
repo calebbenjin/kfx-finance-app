472c19ccec8981ddec0eb90817ff3f8e670bdf6c
calebbenjin <calebben94@gmail.com>
2023-03-08 14:50:33 +0100

    whatsApp link

--- components/supportModal.tsx
+++ components/supportModal.tsx
@@ -29,7 +29,7 @@ const SupportModal = ({isOpen, isClose}:PreviewProps) => {
               <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-light-blue sm:mx-auto my-4 sm:h-20 sm:w-20">
                 <FaWhatsapp className="h-10 w-10 text-blue" aria-hidden="true" />
               </div>
-              <LinkButton href="/" className="inline-flex w-full justify-center items-center rounded-md border border-transparent bg-gradient px-10 sm:px-20 my-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"><FaWhatsapp className="mr-4 h-6 w-6" /> <span>Live Chat</span></LinkButton>
+              <LinkButton href="https://wa.link/oinamx" className="inline-flex w-full justify-center items-center rounded-md border border-transparent bg-gradient px-10 sm:px-20 my-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"><FaWhatsapp className="mr-4 h-6 w-6" /> <span>Live Chat</span></LinkButton>
             </div>
           </div>
         </div>
